417409e06b307e6becf0896735e4a1e4719472fc
Roberto Fraga <me@rober.dev>
2019-05-18 13:24:31 +0200

    Basic functionality

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 const bodyParsre = require('body-parser');
+const axios = require('axios');
+const Fs = require('fs');
+const Path = require('path');
+const base64 = require('node-base64-image');
 
 // Load environment settings
 require('dotenv').config();
@@ -12,9 +16,58 @@ app.get('/', (req, res) => {
   res.json('hi');
 });
 
-app.get('/api/user/:id', (req, res) => {
+app.get('/api/user/:id', async (req, res) => {
   const param = req.params.id;
-  res.json(param);
+  const url = `https://reqres.in/api/users/${param}`;
+  const response = await axios.get(url);
+  res.json(response.data.data);
+});
+
+app.delete('/api/user/:id/avatar', async (req, res) => {
+  const param = req.params.id;
+  const path = Path.resolve(__dirname, 'images', param + '.jpg');
+  const fileExists = await Fs.existsSync(path);
+  if (fileExists) {
+    try {
+      Fs.unlinkSync(path);
+      res.json('File deleted');
+    } catch (err) {
+      res.send(err);
+    }
+  } else {
+    res.json('File does not exists');
+  }
+});
+
+app.get('/api/user/:id/avatar', async (req, res) => {
+  const param = req.params.id;
+  const url = `https://reqres.in/api/users/${param}`;
+  const response = await axios.get(url);
+
+  const avatarUrl = response.data.data.avatar;
+  const path = Path.resolve(__dirname, 'images', param + '.jpg');
+
+  // Search file
+  const fileExists = await Fs.existsSync(path);
+  if (!fileExists) {
+    const writer = Fs.createWriteStream(path);
+
+    const responseImg = await axios({
+      url: avatarUrl,
+      method: 'GET',
+      responseType: 'stream'
+    });
+
+    responseImg.data.pipe(writer);
+  }
+
+  // Response image from disk
+  var img = new Buffer(path, 'base64');
+  res.writeHead(200, {
+    'Content-Type': 'image/jpg',
+    'Content-Length': img.length
+  });
+  res.end(img);
 });
 
 // Start server
