44fa1dc16aab6aac81b37468fdd82d95c1716dd0
Roberto Fraga <me@rober.dev>
2019-05-18 13:55:51 +0200

    Add scrap method

--- index.js
+++ index.js
@@ -70,7 +70,42 @@ app.get('/api/user/:id/avatar', async (req, res) => {
   res.end(img);
 });
 
+app.get('/scrap', async (req, res) => {
+  let url = 'https://reqres.in/api/users?page=';
+  const jsonPath = Path.resolve(__dirname, 'users.json');
+
+  const fileExists = Fs.existsSync(jsonPath);
+  if (!fileExists) {
+    const initialData = { page: 0, data: [] };
+    Fs.writeFileSync(jsonPath, JSON.stringify(initialData));
+  }
+
+  const jsonData = readJsonFileSync(jsonPath);
+  const currentPage = jsonData.page + 1;
+
+  const newData = await axios.get(url + currentPage);
+  if (newData) {
+    newData.data.data.map(item => {
+      jsonData.data.push(item);
+    });
+  }
+  jsonData.page = currentPage;
+
+  var jsonDataStr = JSON.stringify(jsonData);
+  Fs.writeFileSync(jsonPath, jsonDataStr);
+
+  res.json('done');
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+function readJsonFileSync(filepath, encoding) {
+  if (typeof encoding == 'undefined') {
+    encoding = 'utf8';
+  }
+  var file = Fs.readFileSync(filepath, encoding);
+  return JSON.parse(file);
+}
